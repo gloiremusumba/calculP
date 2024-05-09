a9d8d565d5bb24103c92bf83bc3342a39cef65d4
mutsungajames <gloiremusumba1212@gmail.com>
2024-05-09 20:57:42 +0100

    calculatrice en phyton

---
+++ exercice.py
@@ -0,0 +1,16 @@
+nbre1= int(input('entrer un nombre1:'))
+symbole=str(input('entrer un symbole:'))
+nbre2= int(input('entrer un nombre2:'))
+
+if (symbole== "+"):
+    resul=nbre1 + nbre2
+    print (resul)
+elif (symbole== "-"):
+    resul=nbre1 - nbre1
+    print (resul)
+elif (symbole== "*"):
+    resul=nbre1*nbre2
+    print (resul)   
+elif (symbole== "/"):
+    resul=nbre1/nbre2
+    print (resul)    
